65f64f7f4199778f1855af3078c7155732a03c4d
Maria Clara Guimarães <clarareisgui@gmail.com>
2024-01-29 15:27:36 -0300

    ui e tela de simulacao

---
+++ SimulatorView.swift
@@ -0,0 +1,18 @@
+//
+//  SimulatorView.swift
+//  SSC24
+//
+//  Created by Maria Clara Reis Guimarães on 28/01/24.
+//
+
+import SwiftUI
+
+struct SimulatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SimulatorView()
+}
